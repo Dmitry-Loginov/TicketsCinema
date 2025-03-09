9dcbdad9a2986438a8527360a38366cf6db8ad09
Dmitry Loginov <interestingact223@gmail.com>
2025-03-09 22:16:13 +0300

    add users forms styles

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('label').addClass('btn-outline-light text-white');
+    $('button').addClass('btn-outline-light text-white');
+    $('input').addClass('transparent-input .transparent-input::placeholder');
+    $('a').addClass('link-light');
+});
